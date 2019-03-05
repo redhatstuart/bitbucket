a2b760c06393217a822ba9ccf487acfb04aa503f
Stuart R. Kirk <stuart.kirk@microsoft.com>
2019-03-05 18:19:22 -0500

    Added content for SCSI blk-mq enable

--- tuneazure.sh
+++ tuneazure.sh
@@ -55,3 +55,27 @@ then
 else
    echo -e "Accelerated networking does ${RED}*NOT*${NC} appear to be functioning."
 fi
+
+echo " "
+echo " "
+
+echo "Checking if SCSI blk-mq is enabled at boot..."
+echo "*********************************************"
+if [ -z "`grep -i "scsi_mod.use_blk_mq=y" /etc/default/grub`" ]; then
+ echo "SCSI blk-mq is not found. Adding."
+ sed -i 's/GRUB_CMDLINE_LINUX="[^"]*/& scsi_mod.use_blk_mq=Y/' /etc/default/grub
+ echo "Validating that /etc/default/grub has been updated."
+ if [ -n "`grep -i "scsi_mod.use_blk_mq=y" /etc/default/grub`" ]; then
+  echo "Re-making the host grub2.cfg"
+  grub2-mkconfig -o /boot/grub2/grub.cfg >/dev/null 2>&1
+ else
+  echo "Unable to patch grub.cfg. Exiting."
+  exit 1
+ fi
+ echo "Validating that /boot/grub2/grub.cfg has been updated."
+ if [ -n "`grep -i "scsi_mod.use_blk_mq=y" /boot/grub2/grub.cfg`" ]; then
+  echo "Changes to /boot/grub2/grub.cfg are successful and will take effect on next reboot"
+ fi
+else
+ echo "SCSI blk-mq is already enabled on this host."
+fi
